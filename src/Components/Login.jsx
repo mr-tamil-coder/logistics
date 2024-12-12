ddd441b3fc8dd980d56bc64dd0bcb8c9ff59e2eb
vinoth <mohanrajvinoth2004@gmail.com>
2024-12-12 12:08:49 +0530

    udpated design of login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import "./login.css"
+import React from "react";
+import "./login.css";
 const Login = () => {
   return (
-    <div className='bg-gray-400'>
-        <h1>Login</h1>
-        <input type="text" className='input-box' placeholder={"username"}/> <br />
-        <input type="text" className='input-box' placeholder={"Password"} /> <br />
-        <p>
-            Forgot Password
-        </p>
-        Role:
-        <select name="role" id="">
-            <option value="Customer">Customer</option>
-            <option value="Shipper">Shipper</option> <option value="Agents">Agents</option> <option value="Port">Port</option>
-        </select>
-        <br />
-        <p>New user? Create account</p>
-        <button>Login</button>
-    </div>
-  )
-}
+    <div className="bg-gray-400   flex items-center justify-center">
 
-export default Login
+      <div className="bg-red-300">
+        <form action="" className="bg-white p-6"> 
+          <input
+            type="text"
+            className="input-box mb-4 p-2 border border-gray-300 rounded w-full"
+            placeholder={"username"}
+          />{" "}
+          <br />
+          <input
+            type="text"
+            className="input-box mb-4 p-2 border border-gray-300 rounded w-full"
+            placeholder={"Password"}
+          />{" "}
+          <br />
+          <p className="p-3">Forgot Password</p>
+          <div className="role text-center">
+          Role:
+          <select  id="" className="select-role">
+            <option value="Customer">Customer</option>
+            <option value="Shipper">Shipper</option>{" "}
+            <option value="Agents">Agents</option>{" "}
+            <option value="Port">Port</option>
+          </select>
+          </div>
+          <br />
+          <p>New user? Create account</p>
+          <button className=" bg-black text-white py-2 px-4 rounded w-full hover:bg-blue-600">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/Components/SIGUP.jsx
+++ src/Components/SIGUP.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 
-function SIGUP(){
-
-
-    return(
-        <div  className='bg-gray-400'>
-            <form>
-                <input type="text" className="input-box" placeholder="Username"></input><br></br>
-                <input type="password" className="input-box" placeholder="Password"></input><br></br>
-                <input type="email" className="input-box" placeholder="E-mail"></input><br></br>
-               <button className="submit-button">Sign up</button>
-            </form>
-        </div>
-    );
+function SIGNUP() {
+  return (
+    <div className="bg-gray-400 min-h-screen flex items-center justify-center">
+      <form className="bg-white p-6 rounded-lg shadow-lg">
+        <input
+          type="text"
+          className="input-box mb-4 p-2 border border-gray-300 rounded w-full"
+          placeholder="Username"
+        />
+        <br />
+        <input
+          type="password"
+          className="input-box mb-4 p-2 border border-gray-300 rounded w-full"
+          placeholder="Password"
+        />
+        <br />
+        <input
+          type="email"
+          className="input-box mb-4 p-2 border border-gray-300 rounded w-full"
+          placeholder="E-mail"
+        />
+        <br />
+        <button
+          type="submit"
+          className=" bg-black text-white py-2 px-4 rounded w-full hover:bg-blue-600"
+        >
+          Sign up
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default SIGUP
+export default SIGNUP;
