bb9dfed300b238eaa9d52855fc55b3f3ed6a68fc
santhoshghub <santhoshr9663@gmail.com>
2024-12-12 15:41:07 +0530

    view logo

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -5,11 +5,12 @@ import ivw from "./img/logo.png"
   function Logo(){
     return(
         
-        
+        <div className="con">
         <div className="container bg-gray-400">
             <img src={ivw} className="logostyle"></img>
 
         </div> 
+        </div>
  );
   }
    
